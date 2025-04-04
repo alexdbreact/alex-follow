989dd8dd44fd1de767e458f6c0c7ac2a5654aa36
alexdbreact <150357569+alexdbreact@users.noreply.github.com>
2025-04-04 23:35:21 +0200

    Update page.js

--- app/dashboard/vipdashboard/page.js
+++ app/dashboard/vipdashboard/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import AllTable from '@/components/AllVip'
+import AllVip from '@/components/AllVip'
 
 
 
@@ -11,13 +11,13 @@ export default function Dashboard() {
   return (
     <div className="space-y-2 animate-fade-in">
       <div dir='rtl' className="flex justify-between items-center ">
-        <h1 className=" pt-2 text-2xl font-bold text-teal-800 text-center mr-4 ">نتائج الإجتماعات و القرارات و التعليمات الصادرة</h1>
+        <h1 className=" pt-2 text-2xl font-bold text-teal-800 text-center mr-4 "></h1>
         
        
       </div>
 
       <div className="bg-white rounded-lg shadow-lg p-2">
-        <AllTable />
+        <AllVip />
       </div>
 
     
